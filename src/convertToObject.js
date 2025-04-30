71bfd372187dd35337e96b54adb8d213cf73a56e
Kateryna Listratenko <kat.listratenko@gmail.com>
2025-04-30 15:26:10 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,16 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString
+    .split(';') // Розбиваємо рядок по ";"
+    .filter((style) => style.trim()) // Видаляємо пусті елементи
+    .reduce((acc, style) => {
+      const [key, value] = style.split(':').map((str) => str.trim());
+
+      acc[key] = value;
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;
